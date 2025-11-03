6e2d394de5f2777a6b3afde9d0f2d6f7aa50a8db
Hope Calvet <hope.calvet@gmail.com>
2025-11-03 09:59:57 -0500

    Unfinished Smooth Grid Movement
    Chat I'm sorry for not pushing this after our Saturday meeting 😔

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -11,6 +12,12 @@ public class MovementScript : MonoBehaviour
    private Vector2 _smoothMovement;
    private Vector2 _smoothMovementVelocity;
 
+   public bool gridBased;
+   private Vector2 _startMove;
+   private Vector2 _endMove;
+   private int _frameCount;
+   private bool _gridMoveStart;
+
    private void Awake()
    {
       //This is to keep it all private lol lmao even
@@ -22,24 +29,46 @@ public class MovementScript : MonoBehaviour
 
    private void FixedUpdate()
    {
-      //This only happens if you're allowed to move
-      if (GameManager.GM.canMove)
+      if (gridBased)
       {
-         //The smooth movement stuff
-         //SmoothDamp used over Lerp because Lerp looks like ice physics
-         //The smooth time determines how much the player slides
-         _smoothMovement = Vector2.SmoothDamp
-            (_smoothMovement, _movementInput, ref _smoothMovementVelocity, 0.05f);
-      
-         //Where the movement is actually set
-         //This is also why the speed is a variable...to be able to change the speed
-         _playerRigidbody.linearVelocity = _smoothMovement * _speed;
+         _frameCount++;
+         
+         if (GameManager.GM.canMove)
+         {
+            if (_movementInput != Vector2.zero)
+            {
+               //if (_frameCount % 6 == 0)
+               if (!_gridMoveStart)
+               {
+                  _startMove = transform.position;
+                  _endMove = new Vector2(_startMove.x+_movementInput.x,_startMove.y+(_movementInput.y/2));
+                  StartCoroutine(SmoothGridMove());
+                  //transform.position = _endMove;
+               }
+            }
+         }
       }
-      //This is here to stop a bug
-      //Player would keep moving if button was held when clicking on chat box
       else
       {
-         _playerRigidbody.linearVelocity = Vector2.zero;
+         //This only happens if you're allowed to move
+         if (GameManager.GM.canMove)
+         {
+            //The smooth movement stuff
+            //SmoothDamp used over Lerp because Lerp looks like ice physics
+            //The smooth time determines how much the player slides
+            _smoothMovement = Vector2.SmoothDamp
+               (_smoothMovement, _movementInput, ref _smoothMovementVelocity, 0.05f);
+      
+            //Where the movement is actually set
+            //This is also why the speed is a variable...to be able to change the speed
+            _playerRigidbody.linearVelocity = _smoothMovement * _speed;
+         }
+         //This is here to stop a bug
+         //Player would keep moving if button was held when clicking on chat box
+         else
+         {
+            _playerRigidbody.linearVelocity = Vector2.zero;
+         }
       }
    }
    
@@ -47,5 +76,23 @@ public class MovementScript : MonoBehaviour
    {
       //now the input system is all together in 2D movement
       _movementInput = inputValue.Get<Vector2>();
+   }
+
+   IEnumerator SmoothGridMove()
+   {
+      _gridMoveStart = true;
+      float moveTimer = 0;
+
+      while (moveTimer < .25f)
+      {
+         Debug.Log("hi");
+         _smoothMovement = Vector2.SmoothDamp
+            (_smoothMovement, _endMove, ref _smoothMovementVelocity, 0.25f);
+         transform.position = new Vector3(_smoothMovement.x, _smoothMovement.y, _smoothMovement.y);
+         moveTimer += Time.deltaTime;
+      }
+
+      _gridMoveStart = false;
+      yield return new WaitForSeconds(.01f);
    }
 }
