796ac30bea25d04ad4ef1c513ce935d5de7ad3c6
CDeRosa <cderosa179@gmail.com>
2025-11-15 11:35:09 -0500

    Bridges DONE!

--- Assets/Scripts/Bridge.cs
+++ Assets/Scripts/Bridge.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 public class Bridge : MonoBehaviour
 {
     public String nextScene;
+    //the position the player should start at in the next scene
+    public Vector3 sceneStartPos;
     private void OnTriggerEnter2D(Collider2D other)
     {
         print("Bridge Collider has collided with " + other.gameObject.name +"object");
@@ -12,6 +14,8 @@ public class Bridge : MonoBehaviour
         {
             SceneManager.LoadScene(nextScene);
             PlayerInfo.comingFromMediaPortal = false;
+            GameManager.scenePlayerPos = sceneStartPos;
         }
+        
     }
 }
